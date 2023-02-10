dfe61f8bb792c5358d70efc94d23f5c63d6e6af9
VitaliySaburdo <vsaburdo@gmail.com>
2023-02-10 08:44:56 +0200

    12

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -22,6 +22,7 @@ export function ContactForm() {
     }
     dispatch(addContact(name, number));
 
+    // e.target.reset();
     resetForm();
   };
 
@@ -50,6 +51,7 @@ export function ContactForm() {
         <Input
           type="text"
           name="name"
+          value={name}
           onChange={handleNameChange}
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
@@ -61,6 +63,7 @@ export function ContactForm() {
         <Input
           type="tel"
           name="number"
+          value={number}
           onChange={handleNameChange}
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
